47768cc7635912ed6c25710c66a7ca315275eda1
caowenjia <wc2477@columbia.edu>
2018-09-02 19:18:12 +0800

    Update Week1-Plot1.R

--- Week1-Plot1.R
+++ Week1-Plot1.R
@@ -7,7 +7,6 @@ summary(power_cons)
 #Only keep the desired data
 selected<-c("2/2/2007","1/2/2007")
 power_cons1<-power_cons[power_cons$Date %in% selected,]
-power_cons2<-transform(power_cons1,Global_active_power=as.numeric(levels(power_cons1$Global_active_power)))
 
 #hist(power_cons2$Global_active_power[power_cons2$Global_active_power<=6],breaks=seq(0,6,by=0.5),xlim=c(0,6),xlab="Global Active Power (killowatts)",col="red",main="Global Active Power")
 Global_active_power<-as.numeric(levels(power_cons1$Global_active_power))[power_cons1$Global_active_power]
